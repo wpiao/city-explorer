3cbda2329b038801e283ce97399451889524572e
Wenhao Piao <wenhaopiao520@gmail.com>
2021-04-07 22:05:51 -0700

    Complete rendering city map

--- src/City.js
+++ src/City.js
@@ -3,15 +3,17 @@ import { Card } from 'react-bootstrap';
 
 class City extends React.Component {
   render() {
+    const lat = this.props.cityData.lat;
+    const lon = this.props.cityData.lon;
     return (
       <>
         <Card style={{ width: '18rem' }}>
-          {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
           <Card.Body>
             <Card.Title>{this.props.cityData.display_name}</Card.Title>
-            <Card.Text>Latitude: {this.props.cityData.lat}</Card.Text>
-            <Card.Text>Longitude: {this.props.cityData.lon}</Card.Text>
+            <Card.Text>Latitude: {lat}</Card.Text>
+            <Card.Text>Longitude: {lon}</Card.Text>
           </Card.Body>
+          <Card.Img variant="top" src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${lat},${lon}&zoom=12`} />
         </Card>
       </>
     );
