80e2decbf0f948fcb91815c220a1dfcc20be99cc
Wenhao Piao <wenhaopiao520@gmail.com>
2021-04-10 22:32:57 -0700

    Refactor handleSearch function and error handling

--- src/App.js
+++ src/App.js
@@ -9,50 +9,54 @@ import './App.css';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      isSearchedYet: false,
-      cityForSearch: '',
-      isThereError: false,
-      errorMessage: ''
-    };
+    this.state = {};
   }
 
-  handleSearch = async (city) => {
-    try {
-      const response = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${city}&format=json`);
-      const cityData = response.data[0];
-      this.setState({
-        isSearchedYet: true,
-        isThereError: false,
-        cityForSearch: cityData
+  handleSearch = (city) => {
+    axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${city}&format=json`)
+      .then(response => response.data[0])
+      .then(cityData => {
+        this.setState({
+          cityForSearch: cityData,
+          cityErrorMessage: ''
+        });
+        axios.get(`${process.env.REACT_APP_BACKEND_URL}/weather`, {
+          params: {
+            lat: cityData.lat,
+            lon: cityData.lon
+          }
+        })
+          .then(response => {
+            this.setState({
+              weatherData: response.data,
+              weatherErrorMessage: ''
+            });
+          })
+          .catch(error => {
+            this.setState({
+              weatherData: '',
+              weatherErrorMessage: error.message
+            });
+            console.log(error);
+          });
+      })
+      .catch(error => {
+        this.setState({
+          cityForSearch: '',
+          cityErrorMessage: error.message
+        });
       });
-
-      const weatherResponse = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/weather`, {
-        params: {
-          lat: cityData.lat,
-          lon: cityData.lon
-        }
-      });
-      this.setState({
-        weatherData: weatherResponse.data
-      });
-    } catch (error) {
-      this.setState({
-        isSearchedYet: false,
-        isThereError: true,
-        errorMessage: error.toString()
-      });
-    }
-  };
+  }
 
   render() {
     return (
       <div className="city-explorer">
         <h1>City Explorer</h1>
         <Search handleSearch={this.handleSearch} />
-        {this.state.isSearchedYet ? <City cityData={this.state.cityForSearch} /> : ''}
+        {this.state.cityForSearch ? <City cityData={this.state.cityForSearch} /> : ''}
+        {this.state.cityErrorMessage ? <Error cityErrorMessage={this.state.cityErrorMessage} /> : ''}
         {this.state.weatherData ? <Weather data={this.state.weatherData} /> : ''}
-        {this.state.isThereError ? <Error errorMessage={this.state.errorMessage} /> : ''}
+        {this.state.weatherErrorMessage ? <Error weatherErrorMessage={this.state.weatherErrorMessage} /> : ''}
         <footer>&copy; Wenhao Piao</footer>
       </div>
     );
